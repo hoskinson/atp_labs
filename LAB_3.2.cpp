d384509644391907924856a724e5489cc519144d
hoskinson <hoskinson14881@gmail.com>
2020-06-04 17:10:51 +0300

    Lab 3

---
+++ LAB_3.2.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int n;
+	srand(time(0));
+	n = rand() % 91 + 10;
+	cout << "The randomly selected number is :" << n;
+
+	return 0;
+}
