fb66313f7dcfb484ded7f3d2e2ce2edc5d7e7232
hoskinson <hoskinson14881@gmail.com>
2020-06-04 17:03:42 +0300

    Lab 1

---
+++ LAB_1.2.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "\nEnter first number : ";
+	cin >> a;
+	cout << "\nEnter second number : ";
+	cin >> b;
+	c = a + b;
+	cout << "\nThe Sum is : " << c;
+
+
+	return 0;
+}
---
+++ one.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Subject            Marks\n";
+    std::cout << "Mathematics        90 \n";
+    std::cout << "Computer           77 \n";
+    std::cout << "Chemistry          69  solution\n";
+
+}
