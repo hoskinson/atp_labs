ae9d22b18fffd050d4ae4b4a837f56021a56e2f9
hoskinson <hoskinson14881@gmail.com>
2020-06-04 17:12:47 +0300

    Lab 4

---
+++ LAB_4.1.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+
+void showprime(int, int);
+
+int main()
+{
+
+	int x, y;
+	cout << "Enter first  number : ";
+	cin >> x;
+
+	cout << "Enter second  number : ";
+	cin >> y;
+	showprime(x, y);
+
+
+	return 0;
+}
+
+void showprime(int a, int b)
+{
+	bool flag;
+
+	for (int i = a + 1; i <= b; i++)
+	{
+
+		flag = false;
+		for (int j = 2; j < i; j++)
+		{
+
+			if (i % j == 0)
+			{
+				flag = true;
+				break;
+			}
+		}
+
+		if (flag == false && i > 1)
+			cout << i << endl;
+	}
+}
